60bbbe15a633d167d7559bd3701133e6c0dfaef7
jbkunst <jbkunst@gmail.com>
2024-11-21 10:34:01 -0300

    test

--- R/01_data_raster.R
+++ R/01_data_raster.R
@@ -1,8 +1,12 @@
 # packages ---------------------------------------------------------------
 library(earthdatalogin)
 library(gdalcubes)
+library(sf)
+library(glue)
 
 # raster -----------------------------------------------------------------
 edl_netrc(username = Sys.getenv("USERNAME"), password = Sys.getenv("PASSWORD"))
 
 with_gdalcubes()
+
+read_sf(glue("input_data/shp/{site}.gpkg"), layer = "cuartel")
