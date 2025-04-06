2e552e817e12d5c34920e9e4f21dcde0035db2cb
jbkunst <jbkunst@gmail.com>
2025-04-05 22:03:15 -0300

    hist data

--- R/01-data-potencial.R
+++ R/01-data-potencial.R
@@ -2,25 +2,36 @@ source("R/00-funs.R")
 # Process -----------------------------------------------------------------
 cli::cli_h1("Process")
 
-fecha_hoy <- today()
+dates <- today() - 0:1
 
-cli::cli_h2("Download rasters")
-purrr::walk2(sites, rep(fecha_hoy, 2), download_rasters_site_date)
+purrr::walk(dates, safely(function(date = sample(dates, 1)){
 
-cli::cli_h2("Indices")
-purrr::walk(sites, get_indices)
+  cli::cli_h1(as.character(date))
 
-cli::cli_h2("Smoothing")
-purrr::walk(sites, smoothing_rasters, date = fecha_hoy)
+  cli::cli_progress_step(as.character(date))
+  
+  cli::cli_h2("Cleanup")
+  safely(fs::dir_delete)("outputs/")
 
-cli::cli_h2("Climate")
-purrr::walk(sites, get_climate, date = fecha_hoy - days(1))
+  cli::cli_h2("Download rasters")
+  purrr::walk2(sites, rep(date, 2), download_rasters_site_date)
 
-cli::cli_h2("make_prediction_and_save")
-purrr::walk(sites, make_prediction_and_save, date = fecha_hoy)
+  cli::cli_h2("Climate")
+  purrr::walk(sites, get_climate, date = date - days(1))
 
-# cli::cli_h2("Cleanup")
-fs::dir_delete("outputs/")
+  cli::cli_h2("Indices")
+  purrr::walk(sites, get_indices)
+
+  cli::cli_h2("Smoothing")
+  purrr::walk(sites, smoothing_rasters, date = date)
+
+  cli::cli_h2("make_prediction_and_save")
+  purrr::walk(sites, make_prediction_and_save, date = date)
+
+  cli::cli_h2("Cleanup")
+  fs::dir_delete("outputs/")
+
+}))
 
 cli::cli_h2("Creando data/potencial-raster/dates.csv")
 dir("data/potencial-raster", full.names = TRUE, recursive = TRUE) |> 
